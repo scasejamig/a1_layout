f6386d96a3ae560645d2abb4d660f3cd59a7869a
scasejamig <62696085+scasejamig@users.noreply.github.com>
2024-01-19 09:16:34 +0300

    task_1 result
    task_1 result

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>   
+<html lang="ru">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Fin Kit Test</title>
+<link rel="stylesheet" href="css/style.css" type="text/css">
+<?php /*
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
+<link href="https://fonts.cdnfonts.com/css/euclid-circular-a" rel="stylesheet">
+*/ ?>
+<link rel="stylesheet" href="css/css.css?family=Inter">
+<link href="css/euclid-circular-a.css" rel="stylesheet">
+<link rel="stylesheet" href="css/icons.css">
+<link rel="stylesheet" href="css/animation.css"><!--[if IE 7]><link rel="stylesheet" href="css/" + font.fontname + "-ie7.css"><![endif]-->
+<script>
+  function toggleCodes(on) {
+	var obj = document.getElementById('icons');
+  
+	if (on) {
+	  obj.className += ' codesOn';
+	} else {
+	  obj.className = obj.className.replace(' codesOn', '');
+	}
+  }
+</script>
+</head>
+<body>
+
+<div class="main_holder">
+	<div class="work_area_container">
+		<div class="header_area_1">
+			<div class="header_area_select_lang_container_1">
+				<div class="lang_selector_1 lang_selector_2"><a href="#">En</a></div>
+				<div class="lang_selector_1"><a class="lang_selected" href="#">Ru</a></div>				
+			</div>
+			<div class="header_space">
+			</div>
+			
+			<div class="header_slogan_1" id="header_slogan_1_desktop_1">
+				<span class="header_slogan_1_text">Fin</span><span class="header_slogan_2_text">Kit</span>
+				
+				<div class="header_slogan_2">
+					Гибкая мобильная платформа для быстрой реализации инвестиционных сервисов. 
+				</div>
+				
+				<div class="order_demo_button">
+					Заказать демо
+				</div>
+			</div>
+			
+			<div class="hand_with_phone_ellipse">
+				<div class="hand_with_phone_bg">
+					<div class="hand_with_phone">
+						<div class="phone_picture">
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="header_slogan_1" id="header_slogan_1_mobile_1">
+				<span class="header_slogan_1_text">Fin</span><span class="header_slogan_2_text">Kit</span>
+				
+				<div class="header_slogan_2">
+					Гибкая мобильная платформа для быстрой реализации инвестиционных сервисов. 
+				</div>
+				
+				<div class="order_demo_button">
+					Заказать демо
+				</div>
+			</div>
+			
+			<div class="features_1">
+				<div class="features_1_holder" id="features_1_holder_desktop">
+					<div class="feature_1">
+						<div class="feature_1_desc">Гибкая масштабируемая система</div>
+						<div class="feature_1_icon"><i class="demo-icon icon-icon_settings_2 icon_color_1">&#xe803;</i></div>
+					</div>
+					<div class="feature_1">
+						<div class="feature_1_desc">Простая и быстрая интеграция</div>
+						<div class="feature_1_icon"><i class="demo-icon icon-icon_settings_1 icon_color_1">&#xe802;</i></div>
+					</div>
+					<div class="feature_1">
+						<div class="feature_1_desc">Панель администратора для ведения учета</div>
+						<div class="feature_1_icon"><i class="demo-icon icon-icon_line_graphic_1 icon_color_1">&#xe801;</i></div>
+					</div>
+					<div class="feature_1">
+						<div class="feature_1_desc">Удобный пользовательский интерфейс</div>
+						<div class="feature_1_icon"><i class="demo-icon icon-icon_cycle_diagram_1 icon_color_1">&#xe800;</i></div>
+					</div>
+				</div>
+				
+				<div id="features_1_holder_mobile">
+				<div class="features_1_holder">
+					<div class="feature_1">
+						<div class="feature_1_icon"><i class="demo-icon icon-icon_settings_2 icon_color_1">&#xe803;</i></div>
+						<div class="feature_1_desc">Гибкая масштабируемая система</div>						
+					</div>
+					<div class="feature_1">
+						<div class="feature_1_icon"><i class="demo-icon icon-icon_settings_1 icon_color_1">&#xe802;</i></div>
+						<div class="feature_1_desc">Простая и быстрая интеграция</div>						
+					</div>
+				</div>
+				<div class="features_1_holder">
+					<div class="feature_1">
+						<div class="feature_1_icon"><i class="demo-icon icon-icon_line_graphic_1 icon_color_1">&#xe801;</i></div>
+						<div class="feature_1_desc">Панель администратора для ведения учета</div>						
+					</div>
+					<div class="feature_1">
+						<div class="feature_1_icon"><i class="demo-icon icon-icon_cycle_diagram_1 icon_color_1">&#xe800;</i></div>
+						<div class="feature_1_desc">Удобный пользовательский интерфейс</div>						
+					</div>
+				</div>
+				</div>
+			</div>
+			
+			<div class="header_space_2">
+			</div>
+			
+			<div class="key_features_title_1">Наши <span>ключевые продукты</span> для инвестиций</div>
+			
+			<div class="header_space_3">
+			</div>
+			
+			<div class="key_features_2">
+				<div id="key_features_2_holder_desktop">
+					<div class="key_features_2_holder">
+						<div class="key_feature_2">
+							<div class="key_feature_2_desc_holder">
+								<div class="key_feature_2_desc_1">Платформа для автоследования</div>
+								<div class="key_feature_2_desc_2">Копирование стратегий в автономном режиме</div>
+							</div>
+							<div class="key_feature_2_desc_holder">
+								<a href="#"><div class="key_feature_2_picture"></div></a>
+							</div>
+						</div>
+						
+						<div class="key_feature_2">
+							<div class="key_feature_2_desc_holder">
+								<div class="key_feature_2_desc_1">Платформа для робо-эдвайзинга</div>
+								<div class="key_feature_2_desc_2">Персонализированный подбор стратегий исходя из заданных параметров</div>
+							</div>
+							<div class="key_feature_2_desc_holder">
+								<a href="#"><div class="key_feature_2_picture_2"></div></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div id="key_features_2_holder_mobile">
+					<div class="key_features_2_holder">
+						<div class="key_feature_2">
+							<div class="key_feature_2_desc_holder">
+								<div class="key_feature_2_desc_1">Платформа для автоследования</div>
+								<div class="key_feature_2_desc_2">Копирование стратегий в автономном режиме</div>
+							</div>
+							<div class="key_feature_2_desc_holder">
+								<a href="#"><div class="key_feature_2_picture"></div></a>
+							</div>
+						</div>
+					</div>
+					<div class="key_features_2_holder">
+						<div class="key_feature_2">
+							<div class="key_feature_2_desc_holder">
+								<div class="key_feature_2_desc_1">Платформа для робо-эдвайзинга</div>
+								<div class="key_feature_2_desc_2">Персонализированный подбор стратегий исходя из заданных параметров</div>
+							</div>
+							<div class="key_feature_2_desc_holder">
+								<a href="#"><div class="key_feature_2_picture_2"></div></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="header_space_4">
+			</div>
+			
+			<div class="features_desc_3">
+				<div class="key_feature_3_2">
+					<div class="key_features_title_2"><span>Представляем ценность</span> для броĸера и банĸа</div>
+					<div class="features_desc_3_element">
+						Мы помогаем броĸерам и банĸам запустить собственное мобильное приложение для инвестиций в ĸоротĸие сроĸи и с эĸономией бюджета
+					</div>
+					<div class="order_demo_button_2">
+						Заказать демо
+					</div>
+				</div>
+				
+				<div class="key_feature_3_3">
+					<div class="key_feature_4">
+						<i class="demo-icon icon-icon_economy_1 icon_color_2">&#xe804;</i>
+						<div class="features_desc_3_element">
+							Эĸономим время и деньги на запусĸ digital сервисов﻿
+						</div>
+					</div>
+						<div class="key_feature_4">
+						<i class="demo-icon icon-icon_new_clients_1 icon_color_2">&#xe805;</i>
+						<div class="features_desc_3_element">
+							Создаем канал привлечения новых клиентов
+						</div>
+					</div>
+					<div class="key_feature_4">
+						<i class="demo-icon icon-icon_increase_in_income_1 icon_color_2">&#xe806;</i>
+						<div class="features_desc_3_element">
+							Помогаем увеличить выручку путем запуска нового продукта
+						</div>
+					</div>
+						<div class="key_feature_4">
+						<i class="demo-icon icon-icon_increasing_loyalty_1 icon_color_2">&#xe807;</i>
+						<div class="features_desc_3_element">
+							Повышаем лояльность действующих клиентов
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="header_space_4">
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
